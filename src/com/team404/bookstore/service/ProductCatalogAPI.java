4657a0d89690209d77552524d12c3fa66b46dbfe
Wei Wei <weiw4180@gmail.com>
2019-01-01 16:05:49 -0500

    forgot to switch to sub branch

--- src/com/team404/bookstore/service/ProductCatalogAPI.java
+++ src/com/team404/bookstore/service/ProductCatalogAPI.java
@@ -6,11 +6,14 @@ import com.team404.bookstore.dao.DaoFactoryImpl;
 import com.team404.bookstore.entity.BookEntity;
 import com.team404.bookstore.entity.CategoryEntity;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/ProductCatalog")
@@ -19,17 +22,18 @@ public class ProductCatalogAPI {
     private CategoryDao categoryDao;
     private BookDao bookDao;
     private DaoFactoryImpl daoFactory = DaoFactoryImpl.SingleDaoFactory();
+    private static Jsonb jsonb = JsonbBuilder.create();
 
     @GET
     @Path("/getCategoryList")
     @Produces(MediaType.APPLICATION_JSON)
     /* gets the list of product categories for the store */
-    public List<CategoryEntity> getCategoryList() {
+    public Response getCategoryList() {
         categoryDao = new CategoryDao();
 
         List<CategoryEntity> list = categoryDao.ListCategory();
 
-        return list;
+        return Response.status(Response.Status.OK).entity(jsonb.toJson(list)).build();
     }
 
     @GET
